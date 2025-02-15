acef016cc790b5ffd94adeb6ae2adf03511ade2f
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-02-15 20:30:59 +0700

    push

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const RegisterLoginControllers = require("../controllers/RegisterLoginController");
-const ClientControllers = require("../controllers/ClientControllers");
 
 const router = express.Router();
 
 const userRoutes = (app) => {
-  router.get("/api/v1/news/client/show", ClientControllers.showNews);
-  router.patch("/api/v1/news/client/like", ClientControllers.likeNews);
-  router.post("/api/v1/news/client/unlike", ClientControllers.unLikeNews);
-
   router.post("/api/v1/dangky", RegisterLoginControllers.RegisterUser);
   router.post("/api/v1/dangnhap", RegisterLoginControllers.LoginUser);
   router.patch("/api/v1/forgot", RegisterLoginControllers.Forgot);
